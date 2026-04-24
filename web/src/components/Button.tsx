4850b8ddca6fec18bca93c1fda4a102acfeddc61
brianrecoba <brian12fernandes@gmail.com>
2026-04-24 12:49:53 -0300

    feat: create variants in button, created default variant

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import { classMerge } from "../utils/classMerge"
 
 type Props = React.ComponentProps<'button'> & {
-  isDefault?: boolean 
-  children?: React.ReactNode
+    isLoading?: boolean;
+    variant?: 'default' | "cancel" | "delete" | "save"
+    className?: string;
+}
+
+const variants = {
+    button: {
+        "default": 'w-37.5 h-10 bg-primary rounded-full text-white font-monrape font-bold text-[14px] cursor-pointer',
+        "cancel": '',
+        "delete": '',
+        "save": ''
+    }
 }
 
 
-export function Button({ isDefault = true, children, ...props }: Props) {
+export function Button({ children, isLoading, type='button', variant = "default", className,...rest  }: Props) {
     return (
-        <button  {...props}>
+        <button
+          type={type}  
+          {...rest}
+          disabled = {isLoading}
+          className={classMerge([variants.button[variant], className])}
+          >
             {children}
         </button>
     ) 
